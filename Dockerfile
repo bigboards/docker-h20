0839574b37cd861443dd977d5f29a109c9673efd
calmera <daan@bigboards.io>
2016-08-05 11:38:06 +0200

    added H2O_OPTS to add java options to the JVM running H2O

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Pull base image.
+#FROM bigboards/cdh-base-__arch__
+FROM bigboards/java-7-x86_64
+
+MAINTAINER bigboards
+USER root 
+
+# Install much required packages
+RUN apt-get update && apt-get install -f unzip
+
+# Fetch h2o latest_stable
+RUN \
+  wget http://h2o-release.s3.amazonaws.com/h2o/latest_stable -O latest && \
+  wget --no-check-certificate -i latest -O /opt/h2o.zip && \
+  unzip -d /opt /opt/h2o.zip && \
+  rm /opt/h2o.zip && \
+  cd /opt && \
+  cd `find . -name 'h2o.jar' | sed 's/.\///;s/\/h2o.jar//g'` && \ 
+  cp h2o.jar /opt && \
+  wget http://s3.amazonaws.com/h2o-training/mnist/train.csv.gz && \
+  gunzip train.csv.gz 
+
+# Add the executable batch file
+ADD docker_files/h2o-run.sh /apps/h2o-run.sh
+RUN chmod a+x /apps/h2o-run.sh
+
+# declare the volumes
+RUN mkdir /etc/h2o
+VOLUME /etc/h2o
+
+# external ports
+EXPOSE 54321
+
+CMD ["/apps/h2o-run.sh"]
---
+++ docker_files/h2o-run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+CONFIG_FILE=/etc/h2o/h2o-env.sh
+FLAT_FILE=/etc/h2o/flatfile
+
+mask2cidr() {
+    nbits=0
+    IFS=.
+    for dec in $1 ; do
+        case $dec in
+            255) let nbits+=8;;
+            254) let nbits+=7;;
+            252) let nbits+=6;;
+            248) let nbits+=5;;
+            240) let nbits+=4;;
+            224) let nbits+=3;;
+            192) let nbits+=2;;
+            128) let nbits+=1;;
+            0);;
+            *) echo "Error: $dec is not recognised"; exit 1
+        esac
+    done
+    echo "$nbits"
+}
+
+
+# -- Load the configuration file if it exists
+[ -e "$CONFIG_FILE" ] && . ${CONFIG_FILE}
+
+ITF=${H2O_ITF:-wlp2s0}
+
+IP=$(ifconfig ${ITF} | grep "inet addr" | cut -d ':' -f2 | cut -d ' ' -f1)
+MASK=$(ifconfig ${ITF} | grep "Mask" | cut -d ':' -f4)
+CIDR=$(mask2cidr $MASK)
+
+java -Xmx${JAVA_HEAP:-4g} ${H2O_OPTS} -jar /opt/h2o.jar \
+	-name ${H2O_CLOUD_NAME} \
+	-flatfile ${FLAT_FILE} \
+	-network ${IP}/${CIDR}
+
